6900343c2312d58b66de0b27c0e6a83801930b69
VP Nagraj <vpnagraj@virginia.edu>
2017-03-14 14:17:17 -0400

    added demo script to repo

---
+++ code/rpim_demo.R
@@ -0,0 +1,23 @@
+library(data.table)
+library(simpleCache)
+
+# from MIRA
+# parseBiseq
+# BSreadBiSeq
+# extractSampleName ... same as tools::file_path_sans_ext()
+# lapplyAlias
+# setLapplyAlias
+
+# from RPIM
+# relativeProportionOfSites
+# calculatePIM
+# cacheBinomConfInterval
+# BScredInterval
+# BScredIntervalCache
+
+dat <- BSreadBiSeq("RRBS_cpgMethylation_EWS_L10.bed")
+
+# have to setSharedCacheDir()
+setSharedCacheDir("cache")
+
+calculatePIM(dat)
