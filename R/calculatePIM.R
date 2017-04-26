6c84f246a18a905a6aff81ab8eda50fba7d05ea3
VP Nagraj <vpnagraj@virginia.edu>
2017-04-26 13:56:04 -0400

    separated genIM into own function

--- R/calculatePIM.R
+++ R/calculatePIM.R
@@ -3,41 +3,12 @@
 #'
 #' TODO: Make the alpha level a parameter, both for confidence intervals,
 #' and also for IM definition
+#' @export
+
 calculatePIM = function(BSDT) {
 
-  # Grab (or create) the binomial confidence intervals
-  simpleCache("cachedBinomialIntervals95", {
-    cachedBinomialIntervals95 = cacheBinomConfIntervals(2000, 2000, .95)
-  }, cacheDir = getOption("RESOURCES.RCACHE"))
-  cachedBinomialIntervals = cachedBinomialIntervals95
+  imtab = genIM(BSDT)
 
-  # Make the memory use smaller by eliminating unnecessary columns
-  BSDT[,sampleName:=NULL]
-  cachedBinomialIntervals[, method:=NULL]
-  cachedBinomialIntervals[, mean:=NULL]
-  cachedBinomialIntervals[, shape1:=NULL]
-  cachedBinomialIntervals[, shape2:=NULL]
-  cachedBinomialIntervals[, sig:=NULL]
-  cachedBinomialIntervals
+  sum(imtab$IM == TRUE) / nrow(imtab)
 
-  # Calculate the credibility interval
-  CI = BScredIntervalCache(BSDT, cachedBinomialIntervals)
-
-  setkey(CI, "chr", "start")
-
-  # Prep matrix for relative PIM calculation
-  # We define a site as IM (Intermediate Methylation) if its credibility
-  # interval is not completely below .25, or above .75. Other sites are
-  # more likely to be either 0 or 1 (or very close).
-
-  # only keep columns if they exist in input data
-  IM = CI[, IM := !(upper < .25 | lower > .75) ]
-  keepCols = intersect(colnames(CI), c("chr", "start", "id", "IM"))
-
-  IM = CI[, ..keepCols]
-  #IM = CI[, list(chr, start, id, IM = !(upper < .25 | lower > .75)) ]
-
-  # memory hog; clean up!
-  rm(CI); rm(BSDT); gc()
-  return(IM)
 }
---
+++ R/genIM.R
@@ -0,0 +1,40 @@
+#' Given a Bisulfite data.table (BSDT), generates the intermediate methylation (IM) table
+#' @export
+genIM = function(BSDT) {
+
+  # Grab (or create) the binomial confidence intervals
+  simpleCache("cachedBinomialIntervals95", {
+    cachedBinomialIntervals95 = cacheBinomConfIntervals(2000, 2000, .95)
+  }, cacheDir = getOption("RESOURCES.RCACHE"))
+  cachedBinomialIntervals = cachedBinomialIntervals95
+
+  # Make the memory use smaller by eliminating unnecessary columns
+  BSDT[,sampleName:=NULL]
+  cachedBinomialIntervals[, method:=NULL]
+  cachedBinomialIntervals[, mean:=NULL]
+  cachedBinomialIntervals[, shape1:=NULL]
+  cachedBinomialIntervals[, shape2:=NULL]
+  cachedBinomialIntervals[, sig:=NULL]
+  cachedBinomialIntervals
+
+  # Calculate the credibility interval
+  CI = BScredIntervalCache(BSDT, cachedBinomialIntervals)
+
+  setkey(CI, "chr", "start")
+
+  # Prep matrix for relative PIM calculation
+  # We define a site as IM (Intermediate Methylation) if its credibility
+  # interval is not completely below .25, or above .75. Other sites are
+  # more likely to be either 0 or 1 (or very close).
+
+  # only keep columns if they exist in input data
+  IM = CI[, IM := !(upper < .25 | lower > .75) ]
+  keepCols = intersect(colnames(CI), c("chr", "start", "id", "IM"))
+
+  IM = CI[, ..keepCols]
+  #IM = CI[, list(chr, start, id, IM = !(upper < .25 | lower > .75)) ]
+
+  # memory hog; clean up!
+  rm(CI); rm(BSDT); gc()
+  return(IM)
+}
