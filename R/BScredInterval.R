880e3c56c67b306a7245a58908be52c105496c06
VP Nagraj <vpnagraj@virginia.edu>
2017-04-26 13:55:27 -0400

    minor style adjustments to BScredInterval and cacheBinomConfIntervals

--- R/BScredInterval.R
+++ R/BScredInterval.R
@@ -1,8 +1,12 @@
 #' @export
-BScredInterval = function(bsdt, hitCol="hitCount", readCol="readCount", confLevel=.95) {
-  library(binom);
-  conf = binom.bayes(bsdt[,get(hitCol)], bsdt[,get(readCol)], conf.level = confLevel, tol=.005, type="central")
+BScredInterval = function(BSDT, hitCol="hitCount", readCol="readCount", confLevel=.95) {
+
+  conf = binom::binom.bayes(BSDT[,get(hitCol)], BSDT[,get(readCol)], conf.level = confLevel, tol=.005, type="central")
+
   conf = data.table(conf)
-  bsdt = cbind(conf, bsdt)
-  bsdt;
+
+  BSDT = cbind(conf, BSDT)
+
+  BSDT
+
 }
--- R/cacheBinomConfIntervals.R
+++ R/cacheBinomConfIntervals.R
@@ -1,12 +1,10 @@
+#' @export
 
 cacheBinomConfIntervals <- function(maxHits, maxTotal, confLevel) {
-  # library(binom);
 
   allCombinations <- cbind(hits=rep(0:maxHits, each=maxTotal), total=rep(1:maxTotal))
 
   allPossibleCombinations <- allCombinations[allCombinations[,"hits"] <= allCombinations[,"total"],]
-
-  #dim(allPossibleCombinations)
 
   conf <- binom::binom.bayes(allPossibleCombinations[,"hits"], allPossibleCombinations[,"total"], conf.level = confLevel, tol=.005, type="central")
 
