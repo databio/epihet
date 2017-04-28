98977ebfecc3197516393e7e81544cd91eab214e
VP Nagraj <vpnagraj@virginia.edu>
2017-04-28 14:46:31 -0400

    <- to = in cacheBinomConfIntervals

--- R/cacheBinomConfIntervals.R
+++ R/cacheBinomConfIntervals.R
@@ -1,14 +1,14 @@
 #' @export
 
-cacheBinomConfIntervals <- function(maxHits, maxTotal, confLevel) {
+cacheBinomConfIntervals = function(maxHits, maxTotal, confLevel) {
 
-  allCombinations <- cbind(hits=rep(0:maxHits, each=maxTotal), total=rep(1:maxTotal))
+  allCombinations = cbind(hits=rep(0:maxHits, each=maxTotal), total=rep(1:maxTotal))
 
-  allPossibleCombinations <- allCombinations[allCombinations[,"hits"] <= allCombinations[,"total"],]
+  allPossibleCombinations = allCombinations[allCombinations[,"hits"] <= allCombinations[,"total"],]
 
-  conf <- binom::binom.bayes(allPossibleCombinations[,"hits"], allPossibleCombinations[,"total"], conf.level = confLevel, tol=.005, type="central")
+  conf = binom::binom.bayes(allPossibleCombinations[,"hits"], allPossibleCombinations[,"total"], conf.level = confLevel, tol=.005, type="central")
 
-  confdt <- data.table(conf)
+  confdt = data.table(conf)
 
   setkey(confdt, "x", "n")
 
