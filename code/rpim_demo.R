774a602055537a92ea4bc46af9f0ca77b5e7d729
VP Nagraj <vpnagraj@virginia.edu>
2017-05-03 11:48:07 -0400

    more updates to demo script

--- code/rpim_demo.R
+++ code/rpim_demo.R
@@ -1,23 +1,7 @@
-# library(data.table)
-# library(simpleCache)
-
-# from MIRA
-# parseBiseq
-# BSreadBiSeq
-# lapplyAlias
-# setLapplyAlias
-
-# from RPIM
-# relativeProportionOfSites
-# calculatePIM
-# cacheBinomConfInterval
-# BScredInterval
-# BScredIntervalCache
-
 library(RPIM)
 
 dat <- MIRA::BSreadBiSeq("data/RRBS_cpgMethylation_EWS_L10.bed")
-# dat2 <- BSreadBiSeq("data/RRBS_cpgMethylation_EWS_L10.bed")
+
 # have to setSharedCacheDir()
 simpleCache::setSharedCacheDir("cache")
 
@@ -33,8 +17,6 @@ dat4 = MIRA::BSreadBiSeq("data/RRBS_cpgMethylation_EWS_T120.bed")
 
 alldat = rbind(dat,dat2, dat3, dat4)
 allsplitdat = split(alldat, alldat$sampleName)
-
-# relativeProportionOfSites("RRBS_cpgMethylation_EWS_L10", allsplitdat)
 
 calculateRPIM("RRBS_cpgMethylation_EWS_L10", allsplitdat)
 
