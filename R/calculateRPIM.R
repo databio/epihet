fcd944ba3e14611c820ce07408dd17d56a64cf5e
VP Nagraj <vpnagraj@virginia.edu>
2017-05-12 14:50:27 -0400

    new getRPIM() abstracts calculateRPIM() to all samples relative to each other

--- R/calculateRPIM.R
+++ R/calculateRPIM.R
@@ -1,17 +1,13 @@
-
-#' Get the relative proportion of flagged sites. This is a general version
-#' of a method to get the RPIM (Relative Proportion of Intermediate Methylation)
-#' Given a DT with bisulfite reads, and a flag column,
-#' and then a huge table with this data for lots of samples, this will calculate
-#' the relative proportion of CGs that are flagged, for pairwise comparisons
-#' between all samples, subsetted to the CGs present in both samples.
+#' Helper function to get the relative proportion of flagged sites for a single sample versus all other samples in a list of bisulfite data tables.
 #'
 #' @param sampleName The sample (which should specify a name in BSDTlist) to return
 #' the proportion of sites for.
 #' @param BSDTsplit A BSDT (bisulfite data.table) that has been split with
 #' splitDataTable (so, a list of BSDTs); one corresponds to each sample to test.
 calculateRPIM = function(sampleName, BSDTsplit) {
+
   message(sampleName)
+
   result = vector()
 
   sampleBaseline = prepIM(BSDTsplit[[sampleName]])
@@ -21,8 +17,25 @@ calculateRPIM = function(sampleName, BSDTsplit) {
     sampleRelative = prepIM(BSDTsplit[[y]])
     result[y] = merge(sampleBaseline, sampleRelative)[,log(sum(IM.x/.N)/sum(IM.y/.N))]
 
-    # result[y] = merge(BSDTsplit[[sampleName]], BSDTsplit[[y]])[,log(sum(IM.x/.N)/sum(IM.y/.N))]
-
   }
   return(result)
+}
+
+#' Get the relative proportion of flagged sites. This is a general version
+#' of a method to get the RPIM (Relative Proportion of Intermediate Methylation).
+#' Given a DT with bisulfite reads, and a flag column,
+#' and then a huge table with this data for lots of samples, this will calculate
+#' the relative proportion of CGs that are flagged, for pairwise comparisons
+#' between all samples, subsetted to the CGs present in both samples.
+#' @export
+#' @param BSDTsplit A BSDT (bisulfite data.table) that has been split with
+#' splitDataTable (so, a list of BSDTs); one corresponds to each sample to test.
+getRPIM = function(BSDTsplit) {
+
+  x = sapply(names(BSDTsplit, calculateRPIM, BSDTsplit)
+
+  diag(x) = NA
+
+  colMeans(x, na.rm = T)
+
 }
