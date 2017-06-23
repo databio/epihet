04e32d606ba314d7ec071d7590962a0290edfc48
VP Nagraj <vpnagraj@virginia.edu>
2017-06-23 09:15:11 -0400

    minor stylistic changes and docs for interval functions

--- R/intervals.R
+++ R/intervals.R
@@ -1,4 +1,10 @@
-#'@export
+#' Calculate credibility interval based on binomial bayes distribution
+#'
+#' @param BSDT Bisulfite sequencing data in a data.table format
+#' @param hitCol Name of column containing methylation count; defaults to "methylCount"
+#' @param readCol Name of column containing coverage; defaults to "coverage"
+#' @param confLevel The level of confidence to be used in the confidence interval; default is 0.95
+#' @export
 BScredInterval = function(BSDT, hitCol="methylCount", readCol="coverage", confLevel=.95) {
 
   conf = binom::binom.bayes(BSDT[,get(hitCol)], BSDT[,get(readCol)], conf.level = confLevel, tol=.005, type="central")
@@ -11,7 +17,14 @@ BScredInterval = function(BSDT, hitCol="methylCount", readCol="coverage", confLe
 
 }
 
-#'@export
+#' Calculate credibility interval based on binomial bayes distribution with caching
+#'
+#' @param BSDT Bisulfite sequencing data in a data.table format
+#' @param hitCol Name of column containing methylation count; defaults to "methylCount"
+#' @param cachedBinomialIntervals cachedBinomialIntervals
+#' @param readCol Name of column containing coverage; defaults to "coverage"
+#' @param confLevel The level of confidence to be used in the confidence interval; default is 0.95
+#' @export
 BScredIntervalCache = function(BSDT, cachedBinomialIntervals, hitCol="methylCount", readCol="coverage", confLevel=.95){
   storeKey = data.table::key(BSDT)
 
@@ -29,23 +42,28 @@ BScredIntervalCache = function(BSDT, cachedBinomialIntervals, hitCol="methylCoun
 
   BSDT = cachedBinomialIntervals[BSDT,]
 
-  # data.table::setnames(BSDT,c("x", "n"), c("hitCount", "readCount"))
   data.table::setnames(BSDT,c("x", "n"), c(hitCol, readCol))
 
   #And otherwise, count directly.
 
   if(nrow(BSDT[is.na(upper),]) > 0) {
 
-    a= BScredInterval(BSDT[is.na(upper),keepCols, with=FALSE])
+    a = BScredInterval(BSDT[is.na(upper),keepCols, with=FALSE])
 
     BSDT[is.na(upper),] = a[,colnames(BSDT), with=FALSE]
 
   }
 
   BSDT
+
 }
 
-#'@export
+#' Cache binomial confidence intervals
+#'
+#' @param maxHits maxHits
+#' @param maxTotal maxTotal
+#' @param confLevel The level of confidence to be used in the confidence interval; default is 0.95
+#' @export
 cacheBinomConfIntervals = function(maxHits, maxTotal, confLevel) {
 
   allCombinations = cbind(hits=rep(0:maxHits, each=maxTotal), total=rep(1:maxTotal))
@@ -58,5 +76,6 @@ cacheBinomConfIntervals = function(maxHits, maxTotal, confLevel) {
 
   data.table::setkey(confdt, "x", "n")
 
-  return(confdt)
+  confdt
+
 }
