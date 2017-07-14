38bdb4a4a1442025a43f1f6e4787314644c7f46e
VP Nagraj <vpnagraj@virginia.edu>
2017-07-14 16:53:15 -0400

    added another test for object handling

---
+++ tests/testthat/test_objects.R
@@ -0,0 +1,27 @@
+library(RPIM)
+
+context("object handling")
+
+test_that("accept allowed objects for IM calculations", {
+
+    # only first line of message is necessary in this case
+    expect_error(RPIM("foo"), "the following are allowed:\n")
+
+})
+
+test_that("PIM and RPIM warn based on detected number of samples", {
+
+    skip_on_bioc()
+
+    data("exampleBSDT", package = "RPIM")
+    data("BSDTlist", package = "RPIM")
+
+    # PIM errors when given multiple samples
+    expect_error(PIM(BSDTlist, cacheDir = "~"),
+                NULL)
+
+    # RPIM errors when given single sample
+    expect_error(RPIM(exampleBSDT, cacheDir = "~"),
+                 NULL)
+
+})
