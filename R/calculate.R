339b30b88b54c3b102e367698b21d1a5931f0c97
nsheff <nsheff@users.noreply.github.com>
2017-08-16 09:10:58 -0400

    some docs and style cleanup

--- R/calculate.R
+++ R/calculate.R
@@ -123,56 +123,56 @@ calculateRPIM = function(sampleName,
 
 }
 
-#' Get the relative proportion of flagged sites.
+#' Calculate the relative proportion of intermediate methylation (RPIM) score.
 #'
-#' @param bsData Bisulfite sequencing data for multiple samples; a BSDT
-#' (bisulfite data.table) that has been split with splitDataTable
-#' (so, a list of BSDTs); one corresponds to each sample to test.
+#' @param bsData Bisulfite sequencing data for multiple samples. A list of BSDT
+#'     (bisulfite data.table), one corresponds to each sample to test. This may
+#'     also be a BSseq object
 #' @param cacheDir If using caching, this argument specifies the directory to
-#' use for storing the cache;
-#' defaults to global option for \code{RESOURCES.RACHE},
-#' if no such option has been specified you must provide one
-#' @param imLower The lower boundary for intermediate methylation (IM);
-#' if a site is entirely below this threshold
-#' (or if any part of its binomial credibility interval overlaps this boundary)
-#' it is not considered IM;
-#' defaults to .25
-#' @param imUpper The upper boundary for intermediate methylation (IM);
-#' if a site is entirely above this threshold
-#' (or if any part of its binomial credibility interval overlaps this boundary)
-#' it is not considered IM;
-#' defaults to .75
-#' @param confLevel A decimal indicating the level of confidence
-#' to be used while creating cached the binomial bayes credibility interval;
-#' default is .95 for 95 percent confidence
+#'     use for storing the cache; defaults to global option for
+#'     \code{RESOURCES.RACHE}. If no such option has been specified you must
+#'     provide one
+#' @param imLower The lower boundary for intermediate methylation (IM); if a
+#'     site is entirely below this threshold (or if any part of its binomial
+#'     credibility interval overlaps this boundary) it is not considered IM;
+#'     defaults to .25
+#' @param imUpper The upper boundary for intermediate methylation (IM); if a
+#'     site is entirely above this threshold (or if any part of its binomial
+#'     credibility interval overlaps this boundary) it is not considered IM;
+#'     defaults to .75
+#' @param confLevel A decimal indicating the level of confidence to be used
+#'     while creating cached the binomial bayes credibility interval; default is
+#'     .95 for 95 percent confidence
 #'
 #' @return A named vector of the same length as the number of samples being
-#' analyzed; each element in the vector represents the the average proportion of
-#' intermediate methylation relative to the other samples;
-#' all samples are represented and elements named accordingly
+#'     analyzed; each element in the vector represents the the average
+#'     proportion of intermediate methylation relative to the other samples; all
+#'     samples are represented and elements named accordingly
 #'@examples
 #'
-#'data("BSDTlist", package = "RPIM")
+#'data("BSDTlist", package="RPIM")
 #'
 #'if (require(simpleCache)) {
-#'RPIM(BSDTlist, cacheDir = "~")
+#'RPIM(BSDTlist, cacheDir="~")
 #'
 #'simpleCache::setSharedCacheDir("cache")
 #'RPIM(BSDTlist)
-#'RPIM(BSDTlist, imLower = .2, imUpper = .8)
+#'RPIM(BSDTlist, imLower=.2, imUpper=.8)
 #'}
 #' @export
 RPIM = function(bsData,
-                cacheDir = getOption("RESOURCES.RCACHE"),
-                imLower = .25,
-                imUpper = .75,
-                confLevel = .95) {
+                cacheDir=getOption("RESOURCES.RCACHE"),
+                imLower=.25,
+                imUpper=.75,
+                confLevel=.95) {
 
     bsData = bsDataCheck(bsData)
 
     if(singleSample(bsData)) {
 
-        stop("Your data appears to only include one sample. Consider reformatting or try using PIM() to calculate proportion of intermediate methylation for an individual.")
+        stop(strwrap("Your data appears to only include one sample. Consider
+        reformatting or try using PIM() to calculate proportion of intermediate
+        methylation for an individual.", initial="", prefix=" ")
 
     }
 
@@ -186,6 +186,6 @@ RPIM = function(bsData,
 
     diag(x) = NA
 
-    colMeans(x, na.rm = TRUE)
+    colMeans(x, na.rm= RUE)
 
 }
